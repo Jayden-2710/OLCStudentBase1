5e5025a4509e1c267f22a61dc8845ad946deaf18
Jayden-2710 <jayden_lim_1@students.edu.sg>
2026-03-17 10:57:41 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ practice_refinement/sportclub.py
@@ -0,0 +1,23 @@
+'''
+num_members = 15
+for x in range(num_members):
+    sport = input("What is the member's preferred sport?")
+'''
+sports=[]
+while True:
+    sport = input("What is the member's preferred sport?")
+    if sport = "done":
+        break
+    else:
+        sport.append(sports)
+
+x= input('what sport would you like to find?')
+count = 0
+if x in sports:
+ for x in sports:
+          if x == query:
+              countmembers += 1
+
+      print(f"{countmembers} members like the sport {query}.")
+  else:
+      print(f"Nobody likes to play {query}")
